df7a86ce4793f3d031f04079331cd58612be4470
Miguel <miguel.3tp@hotmail.com>
2023-05-01 15:01:13 -0300

    Implementando as restrições de movimentos

--- Xadrez_console/Program.cs
+++ Xadrez_console/Program.cs
@@ -9,52 +9,48 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
                 while (!partida.terminada)
                 {
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
-                    Console.WriteLine();
-                    Console.Write("Origem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno: " + partida.turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-                    Console.WriteLine();
-                    Console.Write("Destino: ");
-                    Posicao destino= Tela.lerPosicaoXadrez().toPosicao();
-                    partida.executaMovimento(origem, destino);
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+
+                    }
                 }
-
-
-
-
-
-
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            /* try { 
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
-
-
-            Tela.imprimirTabuleiro(tab);
             }
             catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
-            }*/
+            }
+
         }
+
     }
+
 }
--- Xadrez_console/tabuleiro/Peca.cs
+++ Xadrez_console/tabuleiro/Peca.cs
@@ -20,6 +20,22 @@ namespace tabuleiro
             this.cor = cor;
             qteMovimentos = 0;
         }
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for(int i=0;i<tab.linhas;i++)
+            {
+                for(int j = 0;j< tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                   
+                }
+            }
+            return false;
+        }
 
         public abstract bool[,] movimentosPossiveis();
       
