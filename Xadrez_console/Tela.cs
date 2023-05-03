0de3a6cef9bee3738fb4273ca7a68ef1b1a83f55
Miguel <miguel.3tp@hotmail.com>
2023-05-02 22:48:17 -0300

    Implementando a logica de xequemate

--- Xadrez_console/Tela.cs
+++ Xadrez_console/Tela.cs
@@ -12,105 +12,116 @@ namespace Xadrez_console
             Console.WriteLine();
             imprimirPecasCapturadas(partida);
             Console.WriteLine("Turno: " + partida.turno);
-            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
-            if (partida.xeque)
+            if (!partida.terminada)
             {
-                Console.WriteLine("XEQUE!");
-            }
-        }
-        public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
-        {
-            Console.WriteLine("Peças Capturadas: ");
-            Console.Write("Brancas: ");
-            imprimirConjunto(partida.pecasCapturada(Cor.Branca));
-            Console.WriteLine();
-            Console.Write("Pretas: ");
-            ConsoleColor aux = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.ForegroundColor = aux;
-            imprimirConjunto(partida.pecasCapturada(Cor.Preta));
-            Console.WriteLine();
-
-        }
-        public static void imprimirConjunto(HashSet <Peca> conjunto)
-        {
-            Console.Write("[");
-            foreach(Peca x in conjunto)
-            {
-                Console.WriteLine(x+" ");
-            }
-            Console.WriteLine("] ");
-        }
-        public static void imprimirTabuleiro(Tabuleiro tab)
-        {
-            for (int i = 0; i < tab.linhas; i++)
-            {
-                Console.Write(8 - i + " ");
-                for (int j = 0; j < tab.colunas; j++)
+                Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                if (partida.xeque)
                 {
-                    imprimirPeca(tab.peca(i,j));
-
+                    Console.WriteLine("XEQUE!");
                 }
-                Console.WriteLine();
-            }
-            Console.WriteLine("  a b c d e f g h ");
-
-        }
-        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoePossiveis)
-        {
-            ConsoleColor fundoOriginal = Console.BackgroundColor;
-            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
-            for (int i = 0; i < tab.linhas; i++)
-            {
-                Console.Write(8 - i + " ");
-                for (int j = 0; j < tab.colunas; j++)
-                {
-                    if (posicoePossiveis[i,j]) {
-                        Console.BackgroundColor = fundoAlterado;
-                    }
-                    else
-                    {
-                        Console.BackgroundColor= fundoOriginal;
-                    }
-                    imprimirPeca(tab.peca(i, j));
-                    Console.BackgroundColor = fundoOriginal;
-
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("  a b c d e f g h ");
-            Console.BackgroundColor = fundoOriginal;
-
-        }
-        public static PosicaoXadrez lerPosicaoXadrez()
-        {
-            string s = Console.ReadLine();
-            char coluna = s[0];
-            int linha = int.Parse(s[1] + "");
-            return new PosicaoXadrez(coluna, linha);
-        }
-        public static void imprimirPeca(Peca peca)
-        {
-            if (peca == null)
-            {
-                Console.Write("- ");
             }
             else
             {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: "+partida.jogadorAtual);
+            }
+        }
 
-                if (peca.cor == Cor.Branca)
+    }
+    public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
+    {
+        Console.WriteLine("Peças Capturadas: ");
+        Console.Write("Brancas: ");
+        imprimirConjunto(partida.pecasCapturada(Cor.Branca));
+        Console.WriteLine();
+        Console.Write("Pretas: ");
+        ConsoleColor aux = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = aux;
+        imprimirConjunto(partida.pecasCapturada(Cor.Preta));
+        Console.WriteLine();
+
+    }
+    public static void imprimirConjunto(HashSet<Peca> conjunto)
+    {
+        Console.Write("[");
+        foreach (Peca x in conjunto)
+        {
+            Console.WriteLine(x + " ");
+        }
+        Console.Write("] ");
+    }
+    public static void imprimirTabuleiro(Tabuleiro tab)
+    {
+        for (int i = 0; i < tab.linhas; i++)
+        {
+            Console.Write(8 - i + " ");
+            for (int j = 0; j < tab.colunas; j++)
+            {
+                imprimirPeca(tab.peca(i, j));
+
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h ");
+
+    }
+    public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoePossiveis)
+    {
+        ConsoleColor fundoOriginal = Console.BackgroundColor;
+        ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+        for (int i = 0; i < tab.linhas; i++)
+        {
+            Console.Write(8 - i + " ");
+            for (int j = 0; j < tab.colunas; j++)
+            {
+                if (posicoePossiveis[i, j])
                 {
-                    Console.Write(peca);
+                    Console.BackgroundColor = fundoAlterado;
                 }
                 else
                 {
-                    ConsoleColor aux = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write(peca);
-                    Console.ForegroundColor = aux;
+                    Console.BackgroundColor = fundoOriginal;
                 }
-                Console.Write(" ");
+                imprimirPeca(tab.peca(i, j));
+                Console.BackgroundColor = fundoOriginal;
+
             }
+            Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h ");
+        Console.BackgroundColor = fundoOriginal;
+
+    }
+    public static PosicaoXadrez lerPosicaoXadrez()
+    {
+        string s = Console.ReadLine();
+        char coluna = s[0];
+        int linha = int.Parse(s[1] + "");
+        return new PosicaoXadrez(coluna, linha);
+    }
+    public static void imprimirPeca(Peca peca)
+    {
+        if (peca == null)
+        {
+            Console.Write("- ");
+        }
+        else
+        {
+
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+            Console.Write(" ");
         }
     }
+}
 }
--- Xadrez_console/xadrez/PartidaDeXadrez.cs
+++ Xadrez_console/xadrez/PartidaDeXadrez.cs
@@ -69,6 +69,10 @@ namespace xadrez
                 xeque = true;
             }
             else { xeque = false; }
+            if (testeXequemate(adversaria(jogadorAtual)))
+            {
+                terminada = true;
+            }
             turno++;
             mudaJogador();
         }
@@ -175,6 +179,36 @@ namespace xadrez
                 }
             }
             return false;
+        }
+        public bool testeXequemate(Cor cor)
+        {
+            if (!estaEmXeque(cor))
+            {
+                return false;
+            }
+            foreach (Peca x in pecasEmJogo(cor))
+            {
+                bool[,] mat = x.movimentosPossiveis();
+                for (int i = 0; i < tab.linhas; i++)
+                {
+                    for (int j = 0; j < tab.colunas; j++)
+                    {
+                        if (mat[i, j])
+                        {
+                            Posicao origem =x.posicao
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(origem, destino, pecaCapturada);
+                            if (!testeXeque)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
         }
 
         public void colocarNovaPeca(char coluna, int linha, Peca peca)
